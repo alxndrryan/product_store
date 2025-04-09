c6fca59d5a4cf1a3fc348313f7b01f8ae6fc0df8
Alexander Hogle <alexanderhogle@gmail.com>
2025-04-09 18:04:59 -0500

    create page adds new product to db

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -8,6 +8,7 @@ import {
 } from '@chakra-ui/react';
 import { useState } from 'react';
 import { useColorModeValue } from '../components/ui/color-mode';
+import { useProductStore } from '../store/product';
 
 const CreatePage = () => {
   const [newProduct, setNewProduct] = useState({
@@ -16,8 +17,12 @@ const CreatePage = () => {
     image: ''
   });
 
-  const handleAddProduct = () => {
-    console.log(newProduct);
+  const { createProduct } = useProductStore();
+
+  const handleAddProduct = async () => {
+    const { success, message } = await createProduct(newProduct);
+    console.log('Success:', success);
+    console.log('Message:', message);
   };
 
   return (
